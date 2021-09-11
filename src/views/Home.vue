ddf56f1f728b963f779d52c2c2ac23b1ed178c6b
Wendy Tsai <>
2021-09-11 22:53:09 +0800

    added Icons folder in components and added every svg icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
     <h1 >text</h1>
-    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
   name: "Home",
   components: {
-  
+    
   },
 };
 </script>
+
