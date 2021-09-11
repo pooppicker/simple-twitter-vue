4c9549aa8d3ed586c33a3e0f402ccfdf64e8215f
jiayenli <x9276550@gmail.com>
2021-09-11 23:16:15 +0800

    navbars第一版製作完成-未重構

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <h1 >text</h1>
-    
+  <div class="container home">
+    <div class="home">
+    <div>
+      <!--navbar-->
+      <NavBars />
+    </div>
+    <div>
+      <!--middle of home-->
+    </div>
+    <div>
+      <!--popular-->
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBars from "./../components/NavBars";
 // @ is an alias to /src
-
 
 export default {
   name: "Home",
   components: {
-  
+    NavBars,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  //標示用
+  border: red 2px solid;
+  .home {
+    max-width: 1440px;
+    background-color:white;
+    margin: 0 auto;
+  }
+}
+</style>
