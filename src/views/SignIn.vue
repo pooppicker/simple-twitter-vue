dabf621bce94c8b0c93cc6acc8d167b5e01c5a14
Wendy Tsai <>
2021-09-12 14:27:18 +0800

    added SignIn Layout

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,66 +1,101 @@
 <template>
-  <div class="container py-5">
+  <div class="container">
     <form class="d-flex flex-column align-items-center">
       <div class="text-center mb-4">
         <AcLogo class="mb-4" />
-        <h1 class="font-weight-normal">
-          登入 Alphitter
-        </h1>
+        <h3 class="mb-4">登入 Alphitter</h3>
       </div>
       <div class="input-style mb-2">
-        <label for="account">帳號</label>
-        <input id="account" type="account" autofocus required>
+        <label class="signin-label" for="account">帳號</label>
+        <input
+          class="signin-input"
+          id="account"
+          type="account"
+          autofocus
+          required
+        />
+        <div v-show="false" class="error-message">帳號不存在</div>
       </div>
-
+      
       <div class="input-style mb-2">
-        <label for="password">密碼</label>
-        <input id="password" type="password" autofocus required>
+        <label class="signin-label" for="password">密碼</label>
+        <input
+          class="signin-input"
+          id="password"
+          type="password"
+          autofocus
+          required
+        />
       </div>
-      <button type="submit" class="btn-signin mb-3">
-        登入
-      </button>
-      <div class="text-center">
+      <button type="submit" class="signin-btn mb-3">登入</button>
+    </form>
+    <template>
+      <div class="link">
         <p>
-          <router-link to="/signup">註冊 Alphitter</router-link>
-          <router-link to="/admin/signin">·後台登入</router-link>
+          <router-link class="siginin-links" to="/signup">註冊 Alphitter</router-link>·<router-link class="siginin-links" to="/admin/signin">後台登入</router-link>
         </p>
       </div>
-    </form>
+    </template>
   </div>
-  
 </template>
 
 <script>
-import AcLogo from "../components/icons/AcLogo.vue"
+import AcLogo from "../components/icons/AcLogo.vue";
 
 export default {
   components: {
-    AcLogo
-  }
-}
+    AcLogo,
+  },
+};
 </script>
 
 <style lang="scss">
 .container {
-  margin: auto auto;
-}
-.input-style {
-  @extend %input-bottom;
-  background-color: #F5F8FA;
-  width: 400px;
-  height: 60px;
-}
-label {
-  font-size: 13px;
-  color: #657786;
-  display: block;
-}
+  margin-top: 4rem;
+  height: 100vh;
+  width: 100vw;
+  .input-style {
+    @extend %input-bottom;
+    background-color: #f5f8fa;
+    width: 450px;
+    height: 52px;
+    margin-bottom: 2rem;
+  }
+  .signin-label {
+    font-size: 13px;
+    color: #657786;
+    display: block;
+  }
+  .signin-input {
+    background-color: #f5f8fa;
+    font-size: 15px;
+    margin-bottom: .5rem;
+  }
+  .signin-btn {
+    width: 450px;
+    height: 40px;
+    color: #ffffff;
+    font-weight: 700;
+    background-color: #ff6600;
+    border-radius: 50px;
+  }
+  .link {
+    position: absolute;
+    left: 54%;
+    p {
+      color: #0099ff;
+      font-size: 13px;
+      font-weight: 700;
+    }
+    .siginin-links {
+      text-decoration: none;
+      color: #0099ff;
+      border-bottom: 1px solid #0099ff;
+      display: inline-block;
+    }
+  }
   
-input {
-  background-color: #F5F8FA;
-  font-size: 19px;
 }
-  
 
-@import "../assets/scss/efficientSetting.scss"
+@import "../assets/scss/efficientSetting.scss";
 </style>
