4b354e980eadbdd8317e79cd9f985ec3ddb36be2
Wendy Tsai <>
2021-09-16 11:59:46 +0800

    added current user to store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,55 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    currentUser: {
+      id: -1,
+      name: "",
+      account: "",
+      email: "",
+      avatar: "",
+      cover: "",
+      introduction: "",
+      TweetsCount: "",
+      FollowersCount: "",
+      FollowingCount: "",
+    },
+    isAuthenticated: false,
+  },
+  mutations: {
+    setCurrentUser(state, currentUser) {
+      state.currentUser = {
+        ...state.currentUser,
+        ...currentUser,
+      }
+      state.isAuthenticated = true
+    }
+  },
+  actions: {
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data } = await usersAPI.getCurrentUser()
+        if (data.status === "error") {
+          throw new Error(data.message)
+        }
+        const { id, name, account, email, avatar, cover, introduction, TweetsCount, FollowersCount, FollowingCount } = data
+        commit("setCurrentUser", {
+          id,
+          name,
+          account,
+          email,
+          avatar,
+          cover,
+          introduction,
+          TweetsCount,
+          FollowersCount,
+          FollowingCount
+        })
+        console.log("data", data)
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  },
   modules: {},
 });
