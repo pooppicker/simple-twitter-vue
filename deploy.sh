320214864901082f45b6358b00654af3601c82d7
Wendy Tsai <>
2021-09-21 00:32:52 +0800

    tried to deploy project

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
-git push -f https://github.com/pooppicker/simple-twitter-vue.git feature-1:gh-pages
+git push -f https://github.com/pooppicker/simple-twitter-vue.git main:gh-pages
 
 cd -
